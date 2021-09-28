15791c16d3c5aea8e60ff208b36d5528c565405b
untilthecore <814898862@qq.com>
2021-09-28 17:33:17 +0800

    Revert "配置根目录"
    This reverts commit 4aa8e710cfafec2ff57080446c469158e1796359.

--- src/index.tsx
+++ src/index.tsx
@@ -1,84 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { createStore, Store } from "redux";
-import { Provider } from "react-redux";
-// import { store } from "./app/store";
-// import App from './App';
-import { devToolsEnhancer } from "redux-devtools-extension";
-import rootReducers, { RootAction, RootState } from "reducers/index";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-// type Action = { type: "INCREMENT" } | { type: "DECREMENT" };
-
-/* function reducers(state = 0, action: Action) {
-  const { type } = action;
-  switch (type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-} */
-
-/* function increment(): Action {
-  return {
-    type: "INCREMENT",
-  };
-} */
-
-/* function decrement(): Action {
-  return {
-    type: "DECREMENT",
-  };
-} */
-
-const store: Store<RootState, RootAction> = createStore(
-  rootReducers /* preloadedState, */,
-  devToolsEnhancer({ trace: true })
-);
-
-console.log(store.getState());
-
-store.subscribe(() => {
-  console.log(store.getState());
-  render();
-});
-
-setTimeout(() => {
-  store.dispatch({
-    type: "ADD_CHAT_LOG",
-    payload: "bbb",
-  });
-}, 1000);
-
-const App: React.FC = () => {
-  return (
-    <>
-      <div>123</div>
-      <button
-        onClick={() =>
-          store.dispatch({
-            type: "ADD_CHAT_LOG",
-            payload: "new log",
-          })
-        }
-      >
-        add log
-      </button>
-      <button>decrement</button>
-    </>
-  );
-};
-
-render();
-
-function render() {
-  ReactDOM.render(
+ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
-    document.getElementById("root")
-  );
-}
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
